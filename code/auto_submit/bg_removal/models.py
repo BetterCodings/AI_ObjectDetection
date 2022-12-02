80806f296ea2319abaa06f5e0c07fc70ef8383c2
HwangJaeMyoung <hjmhjm0329@gmail.com>
2022-12-02 19:55:21 +0900

    DB 수정
    DB 수정

--- code/auto_submit/bg_removal/models.py
+++ code/auto_submit/bg_removal/models.py
@@ -5,4 +5,30 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     image = models.ImageField(default='media/default_image.jpg')
-    
+
+class User(models.Model):
+    name= models.CharField(max_length=30)
+    ID =models.CharField(max_length=30,primary_key=True)
+    Passwd=models.CharField(max_length=30)
+
+class Lecture(models.Model):
+    ID = models.CharField(max_length=30,primary_key=True)
+    subject = models.CharField(max_length=30)
+    pID = models.ForeignKey(User,on_delete=models.CASCADE)
+    sum = models.IntegerField(default=0)
+
+class Class(models.Model):
+    day = models.CharField(max_length=30)
+    preiod = models.CharField(max_length=30)
+    ID =models.ForeignKey(Lecture,on_delete=models.CASCADE)
+    classroom=models.CharField(max_length=30)
+    class Meta:
+        constraints= [models.UniqueConstraint(fields=["day","preiod","ID"],name="Class-unique")]
+
+class Attendence(models.Model):
+    date=models.DateTimeField('date published')
+    ID =models.ForeignKey(Lecture,on_delete=models.CASCADE)
+    attend=models.IntegerField(default=0)
+    image = models.ImageField(default='media/default_image.jpg')
+    class Meta:
+        constraints= [models.UniqueConstraint(fields=["date","ID"],name="Attendence-unique")]
